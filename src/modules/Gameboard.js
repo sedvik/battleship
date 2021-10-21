2062e5c997cecf365e64c224bd14a1447cdfa8cb
Skyler Edvik <edvik.skyler@gmail.com>
2021-10-21 16:06:39 -0600

    Move letterMap and convertCoordinatesToIndices function from Gameboard.js to util.js

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,29 +1,5 @@
 import Ship from './Ship.js'
-
-const letterMap = {
-  A: 0,
-  B: 1,
-  C: 2,
-  D: 3,
-  E: 4,
-  F: 5,
-  G: 6,
-  H: 7,
-  I: 8,
-  J: 9
-}
-
-function convertCoordinatesToIndices (coordinates, letterMap) {
-  // Convert column coordinate from letter to number
-  const letterCol = coordinates.charAt(0)
-  const col = letterMap[letterCol]
-
-  // Downshift row number by 1 so that is 0-indexed instead of starting with 1
-  // const row = parseInt(coordinates.charAt(1)) - 1
-  const row = parseInt(coordinates.slice(1)) - 1
-
-  return { col, row }
-}
+import { letterMap, convertCoordinatesToIndices } from './util.js'
 
 function getShipIndices (frontIndices, orientation, length) {
   const indices = []
---
+++ src/modules/util.js
@@ -0,0 +1,26 @@
+const letterMap = {
+  A: 0,
+  B: 1,
+  C: 2,
+  D: 3,
+  E: 4,
+  F: 5,
+  G: 6,
+  H: 7,
+  I: 8,
+  J: 9
+}
+
+function convertCoordinatesToIndices (coordinates, letterMap) {
+  // Convert column coordinate from letter to number
+  const letterCol = coordinates.charAt(0)
+  const col = letterMap[letterCol]
+
+  // Downshift row number by 1 so that is 0-indexed instead of starting with 1
+  // const row = parseInt(coordinates.charAt(1)) - 1
+  const row = parseInt(coordinates.slice(1)) - 1
+
+  return { col, row }
+}
+
+export { letterMap, convertCoordinatesToIndices }
