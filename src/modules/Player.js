1c48294f9dc0cbc92c0703a980b719d7e299d5ba
Skyler Edvik <edvik.skyler@gmail.com>
2021-10-22 09:51:39 -0600

    Complete Player.js

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,15 +1,33 @@
+import { letterMap, indexMap, convertCoordinatesToIndices, convertIndicesToCoordinates } from './util.js'
+
+function isValidAttack (col, row, enemyGridTracker) {
+  // Return false for undefined row/col
+  if (col === undefined || row === undefined) {
+    return false
+  }
+
+  // return false for spaces that have already been hit
+  const spaceVal = enemyGridTracker[col][row]
+
+  if (spaceVal !== '') {
+    return false
+  }
+
+  return true
+}
+
 const playerPrototype = {
   attack: function (enemyGameboard, coordinate) {
     const result = enemyGameboard.receiveAttack(coordinate)
 
     // Convert coordinate to row and col indices
-    const { row, col } = 3
+    const { row, col } = convertCoordinatesToIndices(coordinate, letterMap)
 
     // Mark enemyGridTracker based on result
     if (result === 'hit') {
-      this.enemyGridTracker
+      this.enemyGridTracker[col][row] = 'X'
     } else if (result === 'miss') {
-
+      this.enemyGridTracker[col][row] = '/'
     }
   }
 }
@@ -38,10 +56,32 @@ function Player (playerType) {
     ['', '', '', '', '', '', '', '', '', '']
   ]
 
-  return Object.assign(Object.create(playerPrototype), {
-    enemyGridTracker,
-    isComputer
-  })
+  const props = {
+    isComputer,
+    enemyGridTracker
+  }
+
+  // Computers gain a randomAttack function
+  if (isComputer) {
+    props.randomAttack = function (enemyGameboard, mathRandomFn) {
+      let colIndex
+      let rowIndex
+
+      // Loop until valid attack is generated
+      while (!isValidAttack(colIndex, rowIndex, this.enemyGridTracker)) {
+        colIndex = Math.floor(mathRandomFn() * 10)
+        rowIndex = Math.floor(mathRandomFn() * 10)
+      }
+
+      // Convert indices to grid coordinates (e.g. A1, F5, etc.)
+      const coordinate = convertIndicesToCoordinates(colIndex, rowIndex, indexMap)
+
+      // Attack enemy gameboard
+      this.attack(enemyGameboard, coordinate)
+    }
+  }
+
+  return Object.assign(Object.create(playerPrototype), props)
 }
 
 export default Player
--- test/Player.test.js
+++ test/Player.test.js
@@ -87,4 +87,125 @@ describe('attack() method', () => {
     expect(mockPlayerGameboard.receiveAttack.mock.calls[0][0]).toBe('B4')
     expect(mockPlayerGameboard.receiveAttack.mock.calls[1][0]).toBe('D5')
   })
+
+  test('attack() on cell F2 and then E9 results in enemyGridTracker being marked with X and / for those positions', () => {
+    player.attack(mockPlayerGameboard, 'F2')
+    player.attack(mockPlayerGameboard, 'E9')
+
+    expect(player.enemyGridTracker[5][1]).toBe('X')
+    expect(player.enemyGridTracker[4][8]).toBe('/')
+  })
+})
+
+describe('randomAttack() method', () => {
+  test('Does not exist for a human player', () => {
+    const player = Player('human')
+    expect(player.randomAttack).toBeUndefined()
+  })
+
+  test('Does exist for a computer player', () => {
+    const player = Player('computer')
+    expect(typeof player.randomAttack).toBe('function')
+  })
+
+  test('randomAttack() calls provided random number generator function twice', () => {
+    const player = Player('computer')
+    const mockEnemyGameboard = {
+      receiveAttack: jest.fn()
+    }
+    mockEnemyGameboard.receiveAttack.mockReturnValueOnce('hit')
+
+    const mockMathRandom = jest.fn()
+    mockMathRandom.mockReturnValueOnce(0.41)
+    mockMathRandom.mockReturnValueOnce(0.74)
+
+    player.randomAttack(mockEnemyGameboard, mockMathRandom)
+
+    expect(mockMathRandom.mock.calls).toHaveLength(2)
+  })
+
+  test('randomAttack() results in gameboard receiveAttack method being called once with generated coordinates', () => {
+    const player = Player('computer')
+
+    const mockEnemyGameboard = {
+      receiveAttack: jest.fn()
+    }
+    mockEnemyGameboard.receiveAttack.mockReturnValueOnce('hit')
+
+    const mockMathRandom = jest.fn()
+
+    // The below mock random values will result in colIndex = 2 and rowIndex = 9 (C10)
+    mockMathRandom.mockReturnValueOnce(0.23)
+    mockMathRandom.mockReturnValueOnce(0.91)
+
+    player.randomAttack(mockEnemyGameboard, mockMathRandom)
+
+    expect(mockEnemyGameboard.receiveAttack).toBeCalledWith('C10')
+  })
+
+  test('randomAttack() results in enemyGridTracker being updated with hit/miss indicators', () => {
+    const player = Player('computer')
+
+    const mockEnemyGameboard = {
+      receiveAttack: jest.fn()
+    }
+    mockEnemyGameboard.receiveAttack.mockReturnValueOnce('hit')
+    mockEnemyGameboard.receiveAttack.mockReturnValueOnce('miss')
+
+    const mockMathRandom = jest.fn()
+
+    // The below mock random values will result in colIndex = 3 and rowIndex = 8 (D9)
+    mockMathRandom.mockReturnValueOnce(0.33)
+    mockMathRandom.mockReturnValueOnce(0.81)
+
+    // The below mock random values will result in colIndex = 5 and rowIndex = 0 (F1)
+    mockMathRandom.mockReturnValueOnce(0.56)
+    mockMathRandom.mockReturnValueOnce(0.03)
+
+    // Hit D9
+    player.randomAttack(mockEnemyGameboard, mockMathRandom)
+
+    // Miss F1
+    player.randomAttack(mockEnemyGameboard, mockMathRandom)
+
+    expect(player.enemyGridTracker[3][8]).toBe('X')
+    expect(player.enemyGridTracker[5][0]).toBe('/')
+  })
+
+  test('randomAttack() cannot attack the same coordinate twice', () => {
+    const player = Player('computer')
+
+    const mockEnemyGameboard = {
+      receiveAttack: jest.fn()
+    }
+    mockEnemyGameboard.receiveAttack.mockReturnValueOnce('hit')
+    mockEnemyGameboard.receiveAttack.mockReturnValueOnce('miss')
+
+    const mockMathRandom = jest.fn()
+
+    // Below mock random values result in colIndex = 2 and rowIndex = 3 (C4)
+    mockMathRandom.mockReturnValueOnce(0.24)
+    mockMathRandom.mockReturnValueOnce(0.37)
+
+    // Below mock random values result in same coordinates as above (C4)
+    mockMathRandom.mockReturnValueOnce(0.21)
+    mockMathRandom.mockReturnValueOnce(0.35)
+
+    // Below mock random values result in colIndex = 8 and rowIndex = 8 (I9)
+    mockMathRandom.mockReturnValueOnce(0.82)
+    mockMathRandom.mockReturnValueOnce(0.85)
+
+    // Hit C4
+    player.randomAttack(mockEnemyGameboard, mockMathRandom)
+
+    // Attempt to hit C4 again, then hit I9
+    player.randomAttack(mockEnemyGameboard, mockMathRandom)
+
+    // To behave as expected, receiveAttack must have only been called twice and mockMathRandom called 6 times (2 for each coordinate generated)
+    expect(mockEnemyGameboard.receiveAttack.mock.calls).toHaveLength(2)
+    expect(mockMathRandom.mock.calls).toHaveLength(6)
+
+    // Last call of receiveAttack should be with I9 instead of C4
+    expect(mockEnemyGameboard.receiveAttack).toHaveBeenLastCalledWith('I9')
+  })
 })
