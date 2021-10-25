fc69bf54cbb37574b9eb009a3b6c5d800bfbae71
Skyler Edvik <edvik.skyler@gmail.com>
2021-10-25 16:08:46 -0600

    Import reset.css and style.css within index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import game from './modules/game.js'
 import displayController from './modules/displayController.js'
+import './css/reset.css'
+import './css/style.css'
 
 displayController.init()
 game.setup()
